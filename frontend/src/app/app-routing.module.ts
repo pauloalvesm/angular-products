944719ff4e65e7c893d233dcbdb416b3e3ce790a
pauloalvesm <paulo.alves7351@gmail.com>
2026-02-17 15:04:08 -0300

    feat: configure home and product routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/pages/home/home.component';
+import { ProductComponent } from './modules/product/components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'products',
+    component: ProductComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
