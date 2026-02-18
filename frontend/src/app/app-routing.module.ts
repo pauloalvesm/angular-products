d56b425e5c41ddfc71ce7f593decf42f016283f9
pauloalvesm <paulo.alves7351@gmail.com>
2026-02-18 09:44:26 -0300

    feat: add component create product and route configuration

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 import { ProductComponent } from './modules/product/components/product/product.component';
+import { ProductCreateComponent } from './modules/product/components/product-create/product-create.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'products',
     component: ProductComponent
+  },
+  {
+    path: 'products/create',
+    component: ProductCreateComponent
   },
 ];
 
--- frontend/src/app/modules/product/product.module.ts
+++ frontend/src/app/modules/product/product.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './components/product/product.component';
+import { ProductCreateComponent } from './components/product-create/product-create.component';
 
 @NgModule({
   declarations: [
-    ProductComponent
+    ProductComponent,
+    ProductCreateComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ProductComponent
+    ProductComponent,
+    ProductCreateComponent
   ]
 })
 export class ProductModule { }
